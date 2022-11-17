3981475cd3a2cc3229ce81c344a5e97112d0f60a
JulianMod <julian.modlinski@gmail.com>
2022-11-17 16:28:06 +0100

    fix bugs and generate fromJson functions

---
+++ packages/poke_api/lib/src/model/generation_IV/sprites.dart
@@ -0,0 +1,38 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../other.dart';
+import '../versions.dart';
+
+part 'sprites.g.dart';
+
+@JsonSerializable()
+class Sprites {
+  Sprites({
+    this.backDefault,
+    this.backFemale,
+    this.backShiny,
+    this.backShinyFemale,
+    this.frontDefault,
+    this.frontFemale,
+    this.frontShiny,
+    this.frontShinyFemale,
+    this.other,
+    this.versions,
+    this.animated,
+  });
+
+  String? backDefault;
+  dynamic? backFemale;
+  String? backShiny;
+  dynamic? backShinyFemale;
+  String? frontDefault;
+  dynamic? frontFemale;
+  String? frontShiny;
+  dynamic? frontShinyFemale;
+  Other? other;
+  Versions? versions;
+  Sprites? animated;
+
+  factory Sprites.fromJson(Map<String, dynamic> json) => _$SpritesFromJson(json);
+
+}
---
+++ packages/poke_api/lib/src/model/pokemon.dart
@@ -0,0 +1,60 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'ability.dart';
+import 'game_index.dart';
+import 'generation_IV/sprites.dart';
+import 'held_item.dart';
+import 'move.dart';
+import 'species.dart';
+import 'stat.dart';
+import 'package:poke_api/src/model/pokemon_type.dart';
+
+part 'pokemon.g.dart';
+
+@JsonSerializable()
+class Pokemon {
+  Pokemon({
+    this.abilities,
+    this.baseExperience,
+    this.forms,
+    this.gameIndices,
+    this.height,
+    this.heldItems,
+    this.id,
+    this.isDefault,
+    this.locationAreaEncounters,
+    this.moves,
+    this.name,
+    this.order,
+    this.pastTypes,
+    this.species,
+    this.sprites,
+    this.stats,
+    this.types,
+    this.weight,
+    this.isFavourite = false
+  });
+
+  List<Ability>? abilities;
+  int? baseExperience;
+  List<Species>? forms;
+  List<GameIndex>? gameIndices;
+  int? height;
+  List<HeldItem>? heldItems;
+  int? id;
+  bool? isDefault;
+  String? locationAreaEncounters;
+  List<Move>? moves;
+  String? name;
+  int? order;
+  List<dynamic>? pastTypes;
+  Species? species;
+  Sprites? sprites;
+  List<Stat>? stats;
+  List<PokemonType>? types;
+  int? weight;
+  @JsonKey(ignore: true)
+  bool isFavourite;
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
+}
