94fc9e599f339fc09128e6b75f880f6bebdfed01
JulianMod <julian.modlinski@gmail.com>
2022-11-17 17:14:55 +0100

    add favourite page

---
+++ lib/favourites/favourites_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FavouritesPage extends StatelessWidget {
+  const FavouritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favourites'),),
+      body: BlocProvider(
+        create: (_) => FavouriteBloc(httpClient: http.Client())..add(FavouriteFetched()),
+        child: const FavouritesList()
+      ),
+    );
+  }
+}
