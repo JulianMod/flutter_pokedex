d6d6740e67915d3fc8f4b6f3f273495fc0196fed
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:35:21 +0100

    add gold model

---
+++ packages/poke_api/src/model/gold.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Gold {
+  Gold({
+    this.backDefault,
+    this.backShiny,
+    this.frontDefault,
+    this.frontShiny,
+    this.frontTransparent,
+  });
+
+  String? backDefault;
+  String? backShiny;
+  String? frontDefault;
+  String? frontShiny;
+  String? frontTransparent;
+
+  factory Gold.fromJson(Map<String, dynamic> json) => _$GoldFromJson(json);
+}
