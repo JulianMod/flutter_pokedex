1b61b59b138284d12c3f49c8ad0f9efa1557acef
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:43:20 +0100

    add dream world model

---
+++ packages/poke_api/src/model/dream_world.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class DreamWorld {
+  DreamWorld({
+    this.frontDefault,
+    this.frontFemale,
+  });
+
+  String frontDefault;
+  dynamic frontFemale;
+
+  factory DreamWorld.fromJson(Map<String, dynamic> json) => _$DreamWorldFromJson(json);
+
+}
