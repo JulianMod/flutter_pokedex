45aa84da5afcbe8fe2c51c132338326f69cbad48
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:44:25 +0100

    add generation VIII model

---
+++ packages/poke_api/src/model/generation_VIII.dart
@@ -0,0 +1,10 @@
+class GenerationViii {
+  GenerationViii({
+    this.icons,
+  });
+
+  DreamWorld? icons;
+
+  factory GenerationViii.fromJson(Map<String, dynamic> json) => _$GenerationViiiFromJson(json);
+
+}
