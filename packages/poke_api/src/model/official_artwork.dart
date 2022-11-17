636236e37d59371c4e02bb13cf5f00a7e9844239
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:47:55 +0100

    add official artwork model

---
+++ packages/poke_api/src/model/official_artwork.dart
@@ -0,0 +1,10 @@
+class OfficialArtwork {
+  OfficialArtwork({
+    this.frontDefault,
+  });
+
+  String? frontDefault;
+
+  factory OfficialArtwork.fromJson(Map<String, dynamic> json) => _$OfficialArtworkFromJson(json);
+
+}
