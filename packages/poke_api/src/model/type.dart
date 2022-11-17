f2a6527735de94a9e918fdfcb12889cbe2e849e2
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:49:25 +0100

    add type model

---
+++ packages/poke_api/src/model/type.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Type {
+  Type({
+    this.slot,
+    this.type,
+  });
+
+  int slot;
+  Species type;
+
+  factory Type.fromJson(Map<String, dynamic> json) => _$TypeFromJson(json);
+
+}
