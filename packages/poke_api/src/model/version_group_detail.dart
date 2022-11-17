89ae083c9c507bd4999fbd9485bce35948660c6e
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:21:02 +0100

    add version group detail model

---
+++ packages/poke_api/src/model/version_group_detail.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class VersionGroupDetail {
+  VersionGroupDetail({
+    this.levelLearnedAt,
+    this.moveLearnMethod,
+    this.versionGroup,
+  });
+
+  int levelLearnedAt;
+  Species moveLearnMethod;
+  Species versionGroup;
+
+  factory VersionGroupDetail.fromJson(Map<String, dynamic> json) => _$VersionGroupDetailFromJson(json);
+}
