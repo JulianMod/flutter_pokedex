fe52ff374dc2939d146ee4f6e1b8af0f47391383
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:39:14 +0100

    add home model

---
+++ packages/poke_api/src/model/home.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Home {
+  Home({
+    this.frontDefault,
+    this.frontFemale,
+    this.frontShiny,
+    this.frontShinyFemale,
+  });
+
+  String? frontDefault;
+  dynamic frontFemale;
+  String? frontShiny;
+  dynamic frontShinyFemale;
+
+  factory Home.fromJson(Map<String, dynamic> json) => _$HomeFromJson(json);
+
+}
