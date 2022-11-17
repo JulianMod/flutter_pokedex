19121a08c6b4373adff2c1a3ed77328a94cec39b
JulianMod <julian.modlinski@gmail.com>
2022-11-17 15:31:49 +0100

    add generation II model

---
+++ packages/poke_api/src/model/generation_II.dart
@@ -0,0 +1,13 @@
+class GenerationIi {
+  GenerationIi({
+    this.crystal,
+    this.gold,
+    this.silver,
+  });
+
+  Crystal? crystal;
+  Gold? gold;
+  Gold? silver;
+
+  factory GenerationIi.fromJson(Map<String, dynamic> json) => _$GenerationIiFromJson(json);
+}
